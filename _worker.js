ee60783c4a36f9019628c4c9033192d442f4ab0a
ljwbh <41594793+ljwbh@users.noreply.github.com>
2024-09-24 16:31:06 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ let mytoken= ['auto'];//快速订阅访问入口, 留空则不启动快速订阅
 
 // 设置优选地址，不带端口号默认443，TLS订阅生成
 let addresses = [
-	'ca.ljwbh.filegear-sg.me:33502#优选域名－A',
+	'ca.ljwbh.filegear-sg.me:27050#优选域名－A',
         //'m.xinpian8.com:8443#m.xinpian8.com',
 	//'www.xinpian8.com:8443#www.xinpian8.com',
 	//'www.yy4080dy.com:8443#www.yy4080dy.com',
